42dd905f3df39a4b68e45ffb7d2a363437ed32eb
dario <kevinramon523@gmail.com>
2022-05-12 23:32:01 -0500

    Add styles

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
 
+//styles
+
+app.use('/static', express.static('public'));
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Guitar Store' });
+  res.render('index', { title: '' });
 });
 //Seccion about
 router.get('/our', function(req, res, next) {
