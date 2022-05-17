d0a55230b32a6b20c072400b68ceba7c7af4f174
dario <kevinramon523@gmail.com>
2022-05-17 15:05:41 -0500

    Resolve model css

--- app.js
+++ app.js
@@ -19,15 +19,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+///
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
-
 //styles
-
-app.use('/static', express.static('public'));
-
+//app.use('/static', express.static('public'));
+app.use(express.static("public"))
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
